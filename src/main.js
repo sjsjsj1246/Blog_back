0751a9137a6b13bbb689c1f29086097a51e4ac5f
Inseo Hwang <sjsjsj1246@gmail.com>
2021-05-11 01:52:26 +0900

    🎨 MongoDB 연동 및 API 리펙토링

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
+
 import api from './api';
+import createFakeData from './createFakeData';
 
 const { PORT, MONGO_URI } = process.env;
 
@@ -11,6 +13,7 @@ mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
   .then(() => {
     console.log('Connected to MongoDB');
+    createFakeData();
   })
   .catch((e) => {
     console.error(e);
