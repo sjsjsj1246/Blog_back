a7ffe7dd87311874a7ce00dca605dd9caed5ed3b
Inseo Hwang <sjsjsj1246@gmail.com>
2021-05-16 17:04:12 +0900

    ✨ Koa-static 정적파일 제공

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
 
 import api from './api';
 import createFakeData from './createFakeData';
@@ -33,6 +36,14 @@ app.use(jwtMiddleware);
 
 //app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(__dirname, '../../blog-front/build');
+app.use(serve(buildDirectory));
+app.use(async (ctx) => {
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
 
 const port = PORT || 4000;
 app.listen(port, () => {
